62f44b66c07b9fa2f22b70f32ebcc4bb64c2628b
Anurag Bodkhe <bodkheanurag235@gmail.com>
2026-06-06 14:37:29 +0530

    updated code

--- Day6/MergeTwoSortedArrays.java
+++ Day6/MergeTwoSortedArrays.java
@@ -2,48 +2,40 @@ package Day6;
 
 class MergeTwoSortedArrays {
 
-    static void merge(int arr1[], int arr2[]) {
-
-        int result[] = new int[arr1.length + arr2.length];
-
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        while (i < arr1.length && j < arr2.length) {
-
-            if (arr1[i] < arr2[j]) {
-                result[k] = arr1[i];
-                i++;
-            } else {
-                result[k] = arr2[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < arr1.length) {
-            result[k] = arr1[i];
-            i++;
-            k++;
-        }
-
-        while (j < arr2.length) {
-            result[k] = arr2[j];
-            j++;
-            k++;
-        }
-
-        for (int x = 0; x < result.length; x++) {
-            System.out.print(result[x] + " ");
-        }
-    }
-
     public static void main(String[] args) {
 
         int arr1[] = {1, 3, 5, 7};
         int arr2[] = {2, 4, 6, 8};
 
-        merge(arr1, arr2);
-    }
+        int n = arr1.length;
+        int m = arr2.length;
+
+        int resultantArr[] = new int[n + m];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < n && j < m) {
+            if (arr1[i]<arr2[j]){
+                resultantArr[k] = arr1[i];
+                i++;
+                k++;
+            }
+            else{
+                resultantArr[k] = arr2[j];
+                j++;
+                k++;
+            }
+
+        }
+        while (i < n) resultantArr[k++] = arr1[i++];
+        while (j < m) resultantArr[k++] = arr2[j++];
+
+        for (i = 0; i < n + m; i++) {
+            System.out.print(resultantArr[i] + " ");
+        }
+
+     }
+
 }
